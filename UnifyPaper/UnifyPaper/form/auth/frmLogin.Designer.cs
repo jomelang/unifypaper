97404c67f58a9e22037090d35cab9a0b85ae7c40
jomelang <jomel.ang@jmc.edu.ph>
2017-09-02 23:09:23 +0800

    buang mo

--- UnifyPaper/UnifyPaper/form/auth/frmLogin.Designer.cs
+++ UnifyPaper/UnifyPaper/form/auth/frmLogin.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.tbUsername = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.tbPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnLogin = new DevComponents.DotNetBar.ButtonX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(32, 48);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(75, 23);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "labelX1";
+            // 
+            // tbUsername
+            // 
+            // 
+            // 
+            // 
+            this.tbUsername.Border.Class = "TextBoxBorder";
+            this.tbUsername.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tbUsername.Location = new System.Drawing.Point(140, 48);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.PreventEnterBeep = true;
+            this.tbUsername.Size = new System.Drawing.Size(100, 22);
+            this.tbUsername.TabIndex = 1;
+            // 
+            // tbPassword
+            // 
+            // 
+            // 
+            // 
+            this.tbPassword.Border.Class = "TextBoxBorder";
+            this.tbPassword.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tbPassword.Location = new System.Drawing.Point(140, 93);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PreventEnterBeep = true;
+            this.tbPassword.Size = new System.Drawing.Size(100, 22);
+            this.tbPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnLogin.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnLogin.Location = new System.Drawing.Point(111, 156);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.labelX1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX tbUsername;
+        private DevComponents.DotNetBar.Controls.TextBoxX tbPassword;
+        private DevComponents.DotNetBar.ButtonX btnLogin;
     }
 }
 
